7259d8512d72f98db5f6d83df5c4a6470544c043
Yeeizu <sergiouv03@educastur.es>
2018-05-04 15:36:40 +0200

    Versión inicial

---
+++ TEPEUVE/Formularios/frmLogin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TEPEUVE
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labUsuario = new System.Windows.Forms.Label();
+            this.labPassword = new System.Windows.Forms.Label();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.cmbUsuario = new System.Windows.Forms.ComboBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.labTitulo = new System.Windows.Forms.Label();
+            this.erpUsuario = new System.Windows.Forms.ErrorProvider(this.components);
+            this.erpPassword = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.erpUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpPassword)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labUsuario
+            // 
+            this.labUsuario.AutoSize = true;
+            this.labUsuario.Location = new System.Drawing.Point(113, 65);
+            this.labUsuario.Name = "labUsuario";
+            this.labUsuario.Size = new System.Drawing.Size(64, 20);
+            this.labUsuario.TabIndex = 0;
+            this.labUsuario.Text = "Usuario";
+            // 
+            // labPassword
+            // 
+            this.labPassword.AutoSize = true;
+            this.labPassword.Location = new System.Drawing.Point(97, 119);
+            this.labPassword.Name = "labPassword";
+            this.labPassword.Size = new System.Drawing.Size(92, 20);
+            this.labPassword.TabIndex = 1;
+            this.labPassword.Text = "Contraseña";
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAcceder.Location = new System.Drawing.Point(0, 185);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(288, 48);
+            this.btnAcceder.TabIndex = 2;
+            this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.UseVisualStyleBackColor = true;
+            // 
+            // cmbUsuario
+            // 
+            this.cmbUsuario.FormattingEnabled = true;
+            this.cmbUsuario.Location = new System.Drawing.Point(13, 88);
+            this.cmbUsuario.Name = "cmbUsuario";
+            this.cmbUsuario.Size = new System.Drawing.Size(264, 28);
+            this.cmbUsuario.TabIndex = 3;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(13, 142);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(264, 26);
+            this.txbPassword.TabIndex = 4;
+            // 
+            // labTitulo
+            // 
+            this.labTitulo.AutoSize = true;
+            this.labTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTitulo.Location = new System.Drawing.Point(67, 9);
+            this.labTitulo.Name = "labTitulo";
+            this.labTitulo.Size = new System.Drawing.Size(148, 31);
+            this.labTitulo.TabIndex = 5;
+            this.labTitulo.Text = "TEPEUVE";
+            // 
+            // erpUsuario
+            // 
+            this.erpUsuario.ContainerControl = this;
+            // 
+            // erpPassword
+            // 
+            this.erpPassword.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 233);
+            this.Controls.Add(this.labTitulo);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.cmbUsuario);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.labPassword);
+            this.Controls.Add(this.labUsuario);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.Text = "Iniciar sesión";
+            ((System.ComponentModel.ISupportInitialize)(this.erpUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpPassword)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labUsuario;
+        private System.Windows.Forms.Label labPassword;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.ComboBox cmbUsuario;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Label labTitulo;
+        private System.Windows.Forms.ErrorProvider erpUsuario;
+        private System.Windows.Forms.ErrorProvider erpPassword;
+    }
+}
+
