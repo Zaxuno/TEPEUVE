4cc1a054257dfd9d795361f78c2b2e88c048e82c
Yeeizu <sergiouv03@educastur.es>
2018-05-08 00:21:14 +0200

    Agregado el formulario del paso 2 de la configuración inicial

---
+++ TEPEUVE/Formularios/frmInstall2.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TEPEUVE.Formularios
+{
+    partial class frmInstall2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.labTitulo = new System.Windows.Forms.Label();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.labNombre = new System.Windows.Forms.Label();
+            this.txbApellidos = new System.Windows.Forms.TextBox();
+            this.labApellidos = new System.Windows.Forms.Label();
+            this.labDNI = new System.Windows.Forms.Label();
+            this.txbDNI = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.btnSiguiente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSiguiente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSiguiente.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSiguiente.FlatAppearance.BorderSize = 0;
+            this.btnSiguiente.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(30)))), ((int)(((byte)(95)))));
+            this.btnSiguiente.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.btnSiguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSiguiente.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(233)))), ((int)(((byte)(212)))));
+            this.btnSiguiente.Location = new System.Drawing.Point(0, 491);
+            this.btnSiguiente.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(786, 75);
+            this.btnSiguiente.TabIndex = 16;
+            this.btnSiguiente.Text = "SIGUIENTE";
+            this.btnSiguiente.UseVisualStyleBackColor = false;
+            // 
+            // labTitulo
+            // 
+            this.labTitulo.AutoSize = true;
+            this.labTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.labTitulo.Font = new System.Drawing.Font("CuprumFFU", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(21)))), ((int)(((byte)(52)))));
+            this.labTitulo.Location = new System.Drawing.Point(188, 9);
+            this.labTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labTitulo.Name = "labTitulo";
+            this.labTitulo.Size = new System.Drawing.Size(410, 71);
+            this.labTitulo.TabIndex = 17;
+            this.labTitulo.Text = "CONFIGURACIÓN";
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.BackColor = System.Drawing.Color.White;
+            this.txbNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.txbNombre.Location = new System.Drawing.Point(13, 129);
+            this.txbNombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(212, 39);
+            this.txbNombre.TabIndex = 20;
+            // 
+            // labNombre
+            // 
+            this.labNombre.AutoSize = true;
+            this.labNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.labNombre.Location = new System.Drawing.Point(13, 93);
+            this.labNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labNombre.Name = "labNombre";
+            this.labNombre.Size = new System.Drawing.Size(99, 31);
+            this.labNombre.TabIndex = 19;
+            this.labNombre.Text = "NOMBRE";
+            // 
+            // txbApellidos
+            // 
+            this.txbApellidos.BackColor = System.Drawing.Color.White;
+            this.txbApellidos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbApellidos.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbApellidos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.txbApellidos.Location = new System.Drawing.Point(233, 129);
+            this.txbApellidos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbApellidos.Name = "txbApellidos";
+            this.txbApellidos.Size = new System.Drawing.Size(540, 39);
+            this.txbApellidos.TabIndex = 22;
+            // 
+            // labApellidos
+            // 
+            this.labApellidos.AutoSize = true;
+            this.labApellidos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.labApellidos.Location = new System.Drawing.Point(233, 93);
+            this.labApellidos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labApellidos.Name = "labApellidos";
+            this.labApellidos.Size = new System.Drawing.Size(121, 31);
+            this.labApellidos.TabIndex = 21;
+            this.labApellidos.Text = "APELLIDOS";
+            // 
+            // labDNI
+            // 
+            this.labDNI.AutoSize = true;
+            this.labDNI.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.labDNI.Location = new System.Drawing.Point(13, 173);
+            this.labDNI.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labDNI.Name = "labDNI";
+            this.labDNI.Size = new System.Drawing.Size(48, 31);
+            this.labDNI.TabIndex = 23;
+            this.labDNI.Text = "DNI";
+            // 
+            // txbDNI
+            // 
+            this.txbDNI.BackColor = System.Drawing.Color.White;
+            this.txbDNI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbDNI.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txbDNI.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.txbDNI.Location = new System.Drawing.Point(13, 209);
+            this.txbDNI.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbDNI.Name = "txbDNI";
+            this.txbDNI.Size = new System.Drawing.Size(212, 39);
+            this.txbDNI.TabIndex = 24;
+            // 
+            // frmInstall2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(153)))), ((int)(((byte)(2)))));
+            this.ClientSize = new System.Drawing.Size(786, 566);
+            this.Controls.Add(this.labDNI);
+            this.Controls.Add(this.txbDNI);
+            this.Controls.Add(this.labApellidos);
+            this.Controls.Add(this.txbApellidos);
+            this.Controls.Add(this.labTitulo);
+            this.Controls.Add(this.labNombre);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.txbNombre);
+            this.Font = new System.Drawing.Font("CuprumFFU", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 8, 5, 8);
+            this.MaximizeBox = false;
+            this.Name = "frmInstall2";
+            this.Text = "Configuración del entorno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Label labTitulo;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.Label labNombre;
+        private System.Windows.Forms.TextBox txbApellidos;
+        private System.Windows.Forms.Label labApellidos;
+        private System.Windows.Forms.Label labDNI;
+        private System.Windows.Forms.TextBox txbDNI;
+    }
+}
---
+++ TEPEUVE/Formularios/frmInstall2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TEPEUVE.Modelos;
+using TEPEUVE.Properties;
+
+//Esta clase corresponde al formulario de configuración para cuando se ejecuta el programa por primera vez.
+namespace TEPEUVE.Formularios
+{
+    public partial class frmInstall2 : Form
+    {
+        //Este objeto corresponde a las operaciones y conexiones con la base de datos local (sqlite).
+        private BaseDeDatos db;
+        //Esta variable recoge el intervalo D,H,M (Dias, Horas y Minutos).
+        char intervalo;
+
+        //Constructor inicial
+        public frmInstall2()
+        {
+            InitializeComponent();
+        }
+    }
+}
